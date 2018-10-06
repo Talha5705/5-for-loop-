302fcfe39669a15daeae265396900ce061d95139
Talha5705 <ztalha@gmail.com>
2018-10-06 16:27:33 -0700

    first commit

---
+++ ForLoop.java
@@ -0,0 +1,21 @@
+
+package forloop;
+
+/**
+ *
+ * @author Talha Zubaer
+ */
+public class ForLoop {
+ 
+    
+    String a;
+    
+    public static void main(String[] args) {
+        String a= "Bangladesh";
+        int i;
+        for(i=0;i<4;i++){
+            System.out.println("\n"+a);
+        }
+    }
+    
+}
